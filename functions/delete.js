fb2bb083b14580bdc1182926fcb2a234f9d4990c
user-1289 <armaanzeyad@gmail.com>
2023-05-08 23:07:32 +0530

    delete option added

--- functions/delete.js
+++ functions/delete.js
@@ -20,7 +20,7 @@ exports.handler = async (event, context) => {
     const filter = { _id: '645686c39e2459f0b58b16f7' };
 // Specify the array field and the value to delete
 const update = { $pull: { Category: delCat } };
-const result = await delCat.updateOne(filter, update);
+const result = await delCol.updateOne(filter, update);
 
   console.log(`Updated ${result.nModified} document(s)`);
 
